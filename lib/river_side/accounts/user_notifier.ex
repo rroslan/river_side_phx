792af9aaa660c363def3164d2fe5f4e05693b831
rroslan <rroslan@gmail.com>
2025-07-28 08:57:51 +0800

    Remove text body from emails, use HTML-only format
    - Simplified email delivery to use only HTML format
    - Removed redundant text body content
    - Cleaner code with single email format
    - HTML emails support all modern email clients

--- lib/river_side/accounts/user_notifier.ex
+++ lib/river_side/accounts/user_notifier.ex
@@ -5,13 +5,12 @@ defmodule RiverSide.Accounts.UserNotifier do
   alias RiverSide.Accounts.User
 
   # Delivers the email using the application mailer.
-  defp deliver(recipient, subject, text_body, html_body) do
+  defp deliver(recipient, subject, html_body) do
     email =
       new()
       |> to(recipient)
       |> from({"River Side Food Court", "noreply@applikasi.tech"})
       |> subject(subject)
-      |> text_body(text_body)
       |> html_body(html_body)
 
     with {:ok, _metadata} <- Mailer.deliver(email) do
@@ -217,22 +216,6 @@ defmodule RiverSide.Accounts.UserNotifier do
   Deliver instructions to update a user email.
   """
   def deliver_update_email_instructions(user, url) do
-    text_body = """
-    Hi #{user.email},
-
-    You requested to change your email address for your River Side Food Court account.
-
-    Please click the link below to confirm your new email:
-    #{url}
-
-    This link will expire in 24 hours.
-
-    If you didn't request this change, please ignore this email or contact our support team.
-
-    Best regards,
-    River Side Food Court Team
-    """
-
     html_body =
       html_template("""
         <div class="content">
@@ -263,7 +246,7 @@ defmodule RiverSide.Accounts.UserNotifier do
         </div>
       """)
 
-    deliver(user.email, "Update your email - River Side Food Court", text_body, html_body)
+    deliver(user.email, "Update your email - River Side Food Court", html_body)
   end
 
   @doc """
@@ -277,22 +260,6 @@ defmodule RiverSide.Accounts.UserNotifier do
   end
 
   defp deliver_magic_link_instructions(user, url) do
-    text_body = """
-    Hi #{user.email},
-
-    Welcome back to River Side Food Court!
-
-    Click the secure link below to log into your account:
-    #{url}
-
-    This magic link will expire in 10 minutes for your security.
-
-    If you didn't request this login link, you can safely ignore this email.
-
-    Enjoy your meal!
-    River Side Food Court Team
-    """
-
     html_body =
       html_template("""
         <div class="content">
@@ -323,32 +290,10 @@ defmodule RiverSide.Accounts.UserNotifier do
         </div>
       """)
 
-    deliver(user.email, "Your login link - River Side Food Court", text_body, html_body)
+    deliver(user.email, "Your login link - River Side Food Court", html_body)
   end
 
   defp deliver_confirmation_instructions(user, url) do
-    text_body = """
-    Hi #{user.email},
-
-    Welcome to River Side Food Court! We're excited to have you join us.
-
-    Please confirm your email address by clicking the link below:
-    #{url}
-
-    Once confirmed, you'll be able to:
-    • Browse menus from all our vendors
-    • Place orders for pickup
-    • Track your order status in real-time
-    • Save your favorite items
-
-    This link will expire in 24 hours.
-
-    If you didn't create an account with us, please ignore this email.
-
-    See you soon!
-    River Side Food Court Team
-    """
-
     html_body =
       html_template("""
         <div class="content">
@@ -390,7 +335,6 @@ defmodule RiverSide.Accounts.UserNotifier do
     deliver(
       user.email,
       "Welcome to River Side Food Court - Please confirm your email",
-      text_body,
       html_body
     )
   end
